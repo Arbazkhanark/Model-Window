962528fd648904bdfd7761dd7d8a0242ac85d5c6
Arbaz Khan <arbaazkhanark23@gmail.com>
2022-07-15 02:09:20 +0530

    created open & close window in the page

--- modelWindow.js
+++ modelWindow.js
@@ -3,19 +3,38 @@
 const model=document.querySelector('.model');
 const overlay=document.querySelector('.overlay');
 const close_btn=document.querySelector('.close-model');
-const open_btn=document.querySelectorAll('.show-btn')
+const open_btn=document.querySelectorAll('.show_btn');
 
   console.log(open_btn);
 
-for(let  i=0;i<open_btn.length();i++){
 
-    console.log(open_btn[i].addEventListener('click', function(){
+//We make function to open the window sheet in the page...  
+const openModel= function(){
+    model.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+};
 
 
+//We make function to close the window sheet in the page... 
+const closeModel=function(){
+model.classList.add('hidden');
+overlay.classList.add('hidden');
+}
 
+ 
+  
+for(let  i=0; i< open_btn.length ; i++){
 
-    }));
+    console.log('Button Clicked')
+    open_btn[i].addEventListener('click', openModel);
+
     
     console.log('hiiiii');
-     
-}
+};
+
+
+//We call function here in both of these lines..
+close_btn.addEventListener('click', closeModel);
+
+overlay.addEventListener('click', closeModel);
+
